6c8c43dbd4f14950542bbbf0070fbe0a795b4a60
brightjun <brightjun98@ajou.ac.kr>
2021-01-24 14:46:20 +0000

    210124_1

--- 1406.py
+++ 1406.py
@@ -1,22 +1,21 @@
-def command_L():
-    df
+import sys
 
-    
-def command_D():
-    
-    
-def command_B():
-    
-    
-def command_P():
-    
-    
-string = input()
-command_num = int(input())
-cursor = len(command_num)
-command = []
 
-for _ in range(command_num):
-    
-    
-    # https://bnzn2426.tistory.com/112
+s1 = list(sys.stdin.readline().strip())
+s2 = []
+m = int(sys.stdin.readline())
+n = len(s1)
+
+for i in range(m):
+    com = sys.stdin.readline().strip()
+    if com[0] == "P":
+        s1.append(com[2])
+    elif com[0] == "L" and s1 != []:
+        s2.append(s1.pop())
+    elif com[0] == "D" and s2 != []:
+        s1.append(s2.pop())
+    elif com[0] == "B" and s1 != []:
+        s1.pop()
+print("".join(s1 + list(reversed(s2))))
+
+# https://namhandong.tistory.com/113
